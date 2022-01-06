d6ac171d050c9e6fb408a4d6ad5fdaaf269ab169
TerranStakers <terran.stakers@protonmail.com>
2022-01-06 17:22:14 +0100

    Frontend from terranseed

---
+++ README.md
@@ -0,0 +1,2 @@
+To get a working map, you need to grab a key in google dashboard, and copy paste it in index.html (look for `key=`)
+
---
+++ src/lib/domain/peer-map-marker.ts
@@ -0,0 +1,7 @@
+import {MapMarker} from "@angular/google-maps";
+import {Peer} from "./peer";
+
+export interface PeerMapMarker extends MapMarker {
+  peers: Peer[];
+  filteredMoniker: string;
+}
